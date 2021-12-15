88538ca3e0a73977054c0249608ab7f8c0326dee
TungSpaceX <doanduytung1602@email.com>
2021-12-15 16:32:22 +0700

    bien

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
-   function welcome() {
-    alert ('welcome to eveyone');
-    
-}
+  var a, b;
+  a = prompt ('nhap gia  tri a');
+  b = prompt ('nhap gia  tri b');
+  a = parseInt ('20');
+  a = parseInt ('10');
+  document.write ("Tong hai so" + a + "va" + b + "la" + (a+b)+);
