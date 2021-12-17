e59a6764c5b20f2820b81a36cc4952d523d05053
TungSpaceX <doanduytung1602@email.com>
2021-12-17 18:39:20 +0700

    baitaptoantu

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
-
+/*
+ <script>
   var a, b;
-  a = prompt ('nhap gia  tri a');
-  b = prompt ('nhap gia  tri b');
-  a = parseInt ('20');
-  a = parseInt ('10');
-  document.write ("Tong hai so" + a + "va" + b + "la" + (a+b)+);
+  a = prompt ("nhap a");
+  b = prompt ("nhap b");
+  a= parseInt (a);
+  b = parseInt (b);
+  document.write ("a + b = " + (a + b) + "<br/>");
+</script>
+<script>
+    let inputWidth;
+    let inputHeight;
+    inputWidth = prompt("Enter the width");
+    inputHeight = prompt("Enter the height");
+    let width = parseInt(inputWidth);
+    let height = parseInt(inputHeight);
+    let area = width * height;
+    document.write("The area  is: " + area);
+</script>
+<script>
+    let day = prompt ('enter the day');
+    let month = prompt ('enter the month');
+    let year = prompt ('enter the year');
+    document.write ("ngay:" + day + "<br/>");
+    document.write ("thang:" + month + "<br/>");
+    document.write ("nam:" + year + "<br/>");
+    document.write ("Ket qua hien thi: " + ( day ) + ( - month ) + ( - year ));
+</script>
+<script>
+    var R; //ban kinh 
+    var Pi = 3.14; //la so pi
+    R = prompt ('nhap R');
+    var R = parseInt (R);
+    document.write ("C = " + (R * Pi) + "<br/>" + " và " + "S = " + (2 * R * Pi));
+</script>
+    var inputinvest;
+    var inputyear;
+    var inputprofit;
+    inputinvest = prompt ('enter the invest');
+    inputyear = prompt ('enter the year');
+    inputprofit = prompt ('enter the profit');
+    inputinvest = parseFloat(inputinvest);
+    inputyear = parseFloat(inputyear);
+    inputprofit = parseFloat(inputprofit);
+    document.write ("invest: " + inputinvest + "<br/>");
+    document.write ("year: " + inputyear + "<br/>");
+    document.write ("profit: " + inputprofit + "<br/>");
+    var area = inputinvest + inputyear * inputprofit;
+    document.write ("result: " + area);
+</script>
+</script>
+    let yourName = prompt ('enter the yourName');
+    let address = prompt ('enter the address');
+    let time = prompt ('enter the time');
+    let content = prompt ('enter the content');
+    document.write ("Ten nguoi nha:" + yourName);
+    document.write ("Dia chi:" + address);
+    document.write ("Thoi gian:" + time);
+    document.write ("Ket qua hien thi:" + content);
+</script>
+*/
